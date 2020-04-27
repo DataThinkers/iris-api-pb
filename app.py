7d79fc11f91dc6529e14be6e1fa00d121f195276
PRIYANG-BHATT <64345839+PRIYANG-BHATT@users.noreply.github.com>
2020-04-27 12:41:33 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model_iris.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+    if output == 0:
+        res_val = "** Iris-Setosa **"
+    elif output==1:
+        res_val = "** Iris-Versicolour **"
+    else: 
+        res_val = "** Iris-Virginica **"
+
+    return render_template('index.html', prediction_text='Flower is :: {}'.format(res_val))
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
